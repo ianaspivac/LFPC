3d7cba28c2571661fa165e929374c20f537159dd
Iana Spivac <yspivac@gmail.com>
2021-03-12 17:25:23 +0200

    Added lab 2

--- Lab 1/FA.py
+++ Lab 1/FA.py
@@ -5,7 +5,7 @@
 Vn = ['S', 'F', 'D']
 Vt = ["a", "b", "c"]
 P = ["S=aF", "F=bF", "F=cD", "S=bS", "D=cS", "D=a$", "F=a$"]
-
+#acbba
 # defining states
 # state which leads to a pair of accepted states and their input
 states = {}
@@ -13,7 +13,7 @@ for el in P:
     if el[0] not in states.keys():
         states[el[0]] = []
     states[el[0]].append([el[-2], el[-1]])
-
+print(states)
 def verify(state, j):
     # a - state input, b - accepted state
     for a, b in state:
